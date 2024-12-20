25329560899b97ad372eb2b425c4b5ec892f8180
deivison556 <deivison10gracindo@outlook.com>
2024-12-20 09:52:03 -0300

    Aula 04 - Variaveis com let - concluido

---
+++ AULAS/Aula 04 - Variaveis com let/index.js
@@ -0,0 +1,23 @@
+let nome = 'João';
+ 
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome, 'em 2015.');
+console.log('O filho de', nome,'se chama Eduardo.');
+
+
+/* Regras para declaração de variaveis 
+
+  - Não podemos criar variáveis com palavaras reservadas
+  - Não pode começar o nome de uma variável com um número
+  - Não podem conter espaços ou traços
+  - As variaveis precisam ter nomes significativos
+  - Utilizamos camelCase para criar variaveis com nome composto
+  - As variavéis são case-sensitive
+  - Não podemos redeclarar variáveis com let
+
+*/
+
+let nomeCompletoDoCliente = 'Deivison Dev'
+console.log(nomeCompletoDoCliente);
