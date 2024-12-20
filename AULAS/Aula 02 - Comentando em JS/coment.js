43be19cc76f43348fbce1d2c58d3679de6c8841c
deivison556 <deivison10gracindo@outlook.com>
2024-12-20 09:17:42 -0300

    Aula 02 - Comentando em JavaScript - concluido

---
+++ AULAS/Aula 02 - Comentando em JS/coment.js
@@ -0,0 +1,15 @@
+// Esse é um comentário simples
+console.log('ola mundo!');
+let nome = 'deivison';
+console.log(nome);
+
+if(nome === 'andria') {
+  console.log('bem-vinda, Andria');
+} else {
+  console.log('ola, outro nome');
+}
+
+/* Esse é um comentário de bloco
+  ainda é comentário de bloco
+*/
+
