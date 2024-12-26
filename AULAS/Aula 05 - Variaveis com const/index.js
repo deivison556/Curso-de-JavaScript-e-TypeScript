374fc2e597ba567f5dd3a576f44ccb75f6edd518
deivison556 <deivison10gracindo@outlook.com>
2024-12-26 09:41:34 -0300

    Aula 05 - Variaveis com const - concluido

---
+++ AULAS/Aula 05 - Variaveis com const/index.js
@@ -0,0 +1,21 @@
+/* Regras para declaração de constante 
+
+  - Não podemos criar constantes com palavaras reservadas
+  - Constantes precisam ter nomes significativos
+  - Não pode começar o nome de uma constante com um número
+  - Não podem conter espaços ou traços
+  - Utilizamos camelCase para criar variaveis com nome composto
+  - As constantes são case-sensitive
+  - Não podemos modificar o valor de uma constante
+  - Não utilize var, utilize const
+
+*/
+
+const nome = 'Deivison';
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
