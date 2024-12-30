0fc783881ba65af839246e032337a9243ec1c77b
deivison556 <deivison10gracindo@outlook.com>
2024-12-30 08:58:03 -0300

    Aula 08 - Operadores aritmeticos em JS

---
+++ AULAS/01 - Modulo JavaScritp básico para iniciantes/Aula 08 - Operadores aritmeticos em js/index.js
@@ -0,0 +1,29 @@
+/* OPERADORES ARITMETICOS
+   
+  + / Adição
+  - / Subtração
+  * / Multiplicação
+  ** / Potenciação
+  % / Resto da divisão
+
+*/
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log(num1 + num2 * num3)
+
+// Operador de Incremento ++
+let contador = 1;
+contador++;
+contador++;
+contador++;
+console.log(contador); //resultado sera 4
+
+// Operador de decremento --
+let segundoContador = 10;
+segundoContador--;
+segundoContador--;
+segundoContador--;
+
+console.log(segundoContador); // resultado sera 7
