3f972bd0496230a78ccd11e67afacee297271c8c
deivison556 <deivison10gracindo@outlook.com>
2025-01-08 11:25:14 -0300

    Aula 12 - Sobre o objeto Math

---
+++ AULAS/01 - Modulo JavaScritp básico para iniciantes/Aula 12 - Sobre o objeto Math/index.js
@@ -0,0 +1,15 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1) // arredondado o número para baixo
+num2 = Math.ceil(num1) // arredondado o número para cima
+num2 = Math.round(num1) // arredondado o número para o mais próximo para cima ou baixo
+console.log(num2);
+
+console.log(Math.max(12,2,3,45,6,100,157,21,-45)); // ele mostra o maior numero da sequencia
+console.log(Math.min(12,2,3,45,6,100,157,21,-45) )// ele mostra o menor numero da sequencia
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio)// gera número aleatórios entre 10 e 5
+console.log(Math.random())// gera número aleatórios entre 0 e 1
+console.log(Math.PI); // mostra o número de pi
+
+
